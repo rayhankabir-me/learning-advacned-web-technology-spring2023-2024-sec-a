60744a0b890931b7cd613449cce075164fc84365
rayhankabir-me <devrayhankabir@gmail.com>
2024-04-16 12:42:39 +0600

    landing page in react app

---
+++ first-react-app/src/Student.tsx
@@ -0,0 +1,11 @@
+export function Student(){
+    return (
+<>
+<tr>
+            <td>Rayhan Kabir</td>
+            <td>21-45109-2</td>
+            <td>Bangladeshi</td>
+        </tr>
+</>
+    )
+}
---
+++ first-react-app/src/Test.tsx
@@ -0,0 +1,5 @@
+export default function Test(){
+    return (
+        <h1>hello world, how are you?</h1>
+    )
+}
